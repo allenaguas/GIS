198adb9ec715e7b938a9b14ca50e713a72ae7be1
Allen Aguas <79226456+allenaguas@users.noreply.github.com>
2024-03-21 11:09:48 -0700

    upload

---
+++ ArcGIS Pro SDK/Identify Features/MapToolIdentify/IdentifyTool.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace MapToolIdentify
+{
+    internal class MapToolIdentify : MapTool
+    {
+        public MapToolIdentify()
+        {
+            IsSketchTool = true;
+            SketchType = SketchGeometryType.Circle;
+            SketchOutputMode = SketchOutputMode.Screen;
+        }
+
+        protected override Task OnToolActivateAsync(bool active)
+        {
+            return base.OnToolActivateAsync(active);
+        }
+
+        protected override async Task<bool> OnSketchCompleteAsync(Geometry geometry)
+        {
+            var mv = MapView.Active;
+            var identifyResult = await QueuedTask.Run(() =>
+            {
+                var sb = new StringBuilder();
+
+                // Get the features that intersect the sketch geometry. 
+                var features = mv.GetFeatures(geometry);
+
+                // Get all layer definitions
+                var lyrs = mv.Map.GetLayersAsFlattenedList().OfType<FeatureLayer>();
+                foreach (var lyr in lyrs)
+                {
+                    var fCnt = features.ToDictionary().ContainsKey(lyr) ? features[lyr].Count : 0;
+                    sb.AppendLine($@"{fCnt} {(fCnt == 1 ? "record" : "records")} for {lyr.Name}");
+                }
+                return sb.ToString();
+            });
+            MessageBox.Show(identifyResult);
+            return true;
+        }
+    }
+}
